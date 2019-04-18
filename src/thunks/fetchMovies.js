287bf53e16dd5be88b11e4061313663312662fc5
Cody-Price <ctprice11@gmail.com>
2019-04-18 11:00:38 -0600

    Initial commit to forked branch

--- src/thunks/fetchMovies.js
+++ src/thunks/fetchMovies.js
@@ -5,7 +5,7 @@ import apiKey from '../apiKey'
 export const fetchMovies = (page) => {
   let selectedPage = page || 1;
   let url = `https://api.themoviedb.org/3/discover/movie?api_key=${apiKey}&language=en-US&sort_by=popularity.desc&include_adult=false&include_video=true&page=${selectedPage}`
-   return async (dispatch) =>  {
+   return async (dispatch) => {
     try {
       dispatch(isLoading(true))
       const response = await fetch(url)
